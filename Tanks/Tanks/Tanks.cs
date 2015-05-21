b3a77e5b41704eeea4107cf1ce29b4c59922237d
Asen Todorov <astodorov@outlook.com>
2015-05-21 20:01:20 +0300

    adding some code to main

--- Tanks/Tanks/Tanks.cs
+++ Tanks/Tanks/Tanks.cs
@@ -1,22 +1,67 @@
 ﻿using System;
 using System.Threading;
 
-class Tanks
+namespace Tanks
 {
-    static void Main()
+
+    class Tanks
     {
-        //Console.Clear();
-        //PrintOnPosition(24, 14, "Go", ConsoleColor.Red);
-        //Thread.Sleep(400);
-        //PrintOnPosition(27, 14, "Go", ConsoleColor.Red);
-        //Thread.Sleep(400);
-        //PrintOnPosition(30, 14, "Go", ConsoleColor.Red);
-        //Thread.Sleep(400);
-        //PrintOnPosition(33, 14, "!", ConsoleColor.Red);
-        //Thread.Sleep(400);
-        //PrintOnPosition(35, 14, "!", ConsoleColor.Red);
-        //Thread.Sleep(400);
-        //PrintOnPosition(37, 14, "!", ConsoleColor.Red);
-        //Thread.Sleep(400);
+        const int GameMenuWidth = 30;
+        const int WindowHeight = 40;
+        const int WindowWidth = 71;
+        const int MaximumEnemies = 10;
+        static void Main()
+        {
+            Console.BufferHeight = Console.WindowHeight = 40;
+            Console.BufferWidth = Console.WindowWidth = 71;
+            int boundaryX = 71;
+            int boundaryY = Console.BufferHeight;
+            Console.CursorVisible = false;
+            Tank ourTank = new Tank(boundaryX / 2, boundaryY);
+
+            //DrawGameMenu();
+
+            while (true)
+            {
+                Console.Clear();
+                while (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                    while (Console.KeyAvailable) Console.ReadKey(true);
+                    if (pressedKey.Key == ConsoleKey.LeftArrow)
+                    {
+                        if (ourTank.X - 1 >= 0)
+                        {
+                            ourTank.X = ourTank.X - 1;
+                        }
+                    }
+                    else if (pressedKey.Key == ConsoleKey.RightArrow)
+                    {
+                        if (ourTank.X + 3 < 40)
+                        {
+                            ourTank.X = ourTank.X + 1;
+                        }
+                    }
+                    else if (pressedKey.Key == ConsoleKey.RightArrow)
+                    {
+                        if (ourTank.X + 3 < 40)
+                        {
+                            ourTank.X = ourTank.X + 1;
+                        }
+                    }
+                }
+                ourTank.Draw();
+                Thread.Sleep(150);
+            }
+        }
+        private static void DrawGameMenu()
+        {
+        }
+        static void PrintOnPosition(int x, int y, string str, ConsoleColor color = ConsoleColor.DarkBlue) // for strings
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(str);
+        }
     }
 }
