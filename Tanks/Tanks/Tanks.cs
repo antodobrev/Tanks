2df18a76e438f30c88f825bd591662563f0ba289
Ivan <88ivanov11@gmail.com>
2015-05-21 19:50:05 +0300

    Implementing game object

--- Tanks/Tanks/Tanks.cs
+++ Tanks/Tanks/Tanks.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Threading;
 
 class Tanks
 {
     static void Main()
     {
-        Console.WriteLine("This is Tank's Sparta");
+        Console.Clear();
+        PrintOnPosition(24, 14, "Go", ConsoleColor.Red);
+        Thread.Sleep(400);
+        PrintOnPosition(27, 14, "Go", ConsoleColor.Red);
+        Thread.Sleep(400);
+        PrintOnPosition(30, 14, "Go", ConsoleColor.Red);
+        Thread.Sleep(400);
+        PrintOnPosition(33, 14, "!", ConsoleColor.Red);
+        Thread.Sleep(400);
+        PrintOnPosition(35, 14, "!", ConsoleColor.Red);
+        Thread.Sleep(400);
+        PrintOnPosition(37, 14, "!", ConsoleColor.Red);
+        Thread.Sleep(400);
     }
 }
