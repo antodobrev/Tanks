5b42e89a1f2614a68f3cd4924bd85f8b7166dcb0
Ivan <88ivanov11@gmail.com>
2015-05-21 19:56:10 +0300

    Implementing game objects

---
+++ Tanks/Tanks/GameObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public interface GameObject
+{
+    int X
+    {
+        get;
+    }
+
+    int Y
+    {
+        get;
+    }
+
+    ConsoleColor Color
+    {
+        get;
+    }
+
+    void Draw();
+}
--- Tanks/Tanks/Tanks.cs
+++ Tanks/Tanks/Tanks.cs
@@ -5,18 +5,18 @@ class Tanks
 {
     static void Main()
     {
-        Console.Clear();
-        PrintOnPosition(24, 14, "Go", ConsoleColor.Red);
-        Thread.Sleep(400);
-        PrintOnPosition(27, 14, "Go", ConsoleColor.Red);
-        Thread.Sleep(400);
-        PrintOnPosition(30, 14, "Go", ConsoleColor.Red);
-        Thread.Sleep(400);
-        PrintOnPosition(33, 14, "!", ConsoleColor.Red);
-        Thread.Sleep(400);
-        PrintOnPosition(35, 14, "!", ConsoleColor.Red);
-        Thread.Sleep(400);
-        PrintOnPosition(37, 14, "!", ConsoleColor.Red);
-        Thread.Sleep(400);
+        //Console.Clear();
+        //PrintOnPosition(24, 14, "Go", ConsoleColor.Red);
+        //Thread.Sleep(400);
+        //PrintOnPosition(27, 14, "Go", ConsoleColor.Red);
+        //Thread.Sleep(400);
+        //PrintOnPosition(30, 14, "Go", ConsoleColor.Red);
+        //Thread.Sleep(400);
+        //PrintOnPosition(33, 14, "!", ConsoleColor.Red);
+        //Thread.Sleep(400);
+        //PrintOnPosition(35, 14, "!", ConsoleColor.Red);
+        //Thread.Sleep(400);
+        //PrintOnPosition(37, 14, "!", ConsoleColor.Red);
+        //Thread.Sleep(400);
     }
 }
